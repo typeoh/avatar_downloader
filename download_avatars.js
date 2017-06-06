95cd3bd70d4dc27f59936620aca88cb6911aff60
Shauna Griffin <shaunahennessygriffin@gmail.com>
2017-06-06 04:51:52 +0000

    added support for command line arguments owner and repo

--- download_avatars.js
+++ download_avatars.js
@@ -2,12 +2,13 @@ var request = require('request');
 var GITHUB_USER = "typeoh";
 var GITHUB_TOKEN = "1ea64e31096f5c92dd23e5cbe53fdb84456e3a0c"
 var fs = require('fs');
-var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + "jquery" + '/' + "jquery" + '/contributors';
-
+var owner = process.argv[2];
+var repo = process.argv[3];
 //cb helps return the asynchronous results of getRepoContributors
 //call back function going
 
 function getRepoContributors(repoOwner, repoName, cb) {
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
   var options = {
       url: requestURL,
       headers: {
@@ -17,13 +18,14 @@ function getRepoContributors(repoOwner, repoName, cb) {
   }
   request.get(options, function(err, response, body) {              // Note 1
     cb(err, body);
+    // console.log(body);
   });
 
 };
 //chaining style request that pipes the image
-getRepoContributors("jquery", "jquery", function(err, result) {
+getRepoContributors(owner, repo, function(err, result) {
     result.forEach(function(contributor) {
-      var urlCb = contributor.avatar_url
+      var urlCb = contributor.avatar_url;
       var pathCb = "avatars/" + contributor.login + ".jpg";
       downloadImageByURL(urlCb, pathCb);
       // console.log(contributor.login + " : " + contributor.avatar_url)
