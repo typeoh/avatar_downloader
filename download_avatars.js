067fcfa0c8080e66685d871c2f851fc1fa69bfc1
Shauna Griffin <shaunahennessygriffin@gmail.com>
2017-06-06 04:30:50 +0000

    piped image from url into folder avatars

--- download_avatars.js
+++ download_avatars.js
@@ -1,7 +1,7 @@
 var request = require('request');
 var GITHUB_USER = "typeoh";
 var GITHUB_TOKEN = "1ea64e31096f5c92dd23e5cbe53fdb84456e3a0c"
-
+var fs = require('fs');
 var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + "jquery" + '/' + "jquery" + '/contributors';
 
 //cb helps return the asynchronous results of getRepoContributors
@@ -18,21 +18,19 @@ function getRepoContributors(repoOwner, repoName, cb) {
   request.get(options, function(err, response, body) {              // Note 1
     cb(err, body);
   });
-       // .on('error', function (err) {                                   // Note 2
-       //   throw err;
-       // })
-       // .on('response', function (response) {                           // Note 3
-       //   console.log('Response Status Code: ', response.statusCode);
-       //  })
 
 };
 //chaining style request that pipes the image
-(getRepoContributors("jquery", "jquery", function(err, result) {
+getRepoContributors("jquery", "jquery", function(err, result) {
     result.forEach(function(contributor) {
-      console.log(contributor.login + " : " + contributor.avatar_url)
+      var urlCb = contributor.avatar_url
+      var pathCb = "avatars/" + contributor.login + ".jpg";
+      downloadImageByURL(urlCb, pathCb);
+      // console.log(contributor.login + " : " + contributor.avatar_url)
     });
+});
 
-  // console.log("Errors:", err);
-  // console.log("Result:", result);
-}));
-
+function downloadImageByURL(url, filePath) {
+  request(url)
+  .pipe(fs.createWriteStream(filePath));
+}
